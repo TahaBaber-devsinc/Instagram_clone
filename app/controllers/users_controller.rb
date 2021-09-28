98446474c42ef1156197857a0eb50ab4d240f604
TahaBaber-devsinc <87317554+TahaBaber-devsinc@users.noreply.github.com>
2021-09-28 15:56:54 +0500

    Changes for exception

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@
 # class for users
 class UsersController < ApplicationController
   before_action :authorize_user
+  rescue_from ActiveRecord::ActiveRecordError, with: :handle_exception
 
   def index
     @users = params[:search].blank? ? User.all : User.search(params[:search])
@@ -16,13 +17,15 @@ class UsersController < ApplicationController
   def update
     Request.where(followee_id: current_user.id).each { |request| RequestAccept.new(request.user, current_user).call }
     current_user.update(account_type: User.account_types[current_user.account_type] ^ 1)
-    rescue ActiveRecord::ActiveRecordError => e
-       redirect_to current_user, flash: { notice: e.message }
   end
 
   private
 
   def authorize_user
     authorize(current_user, policy_class: UserPolicy)
+  end
+
+  def handle_exception(exception)
+    redirect_to current_user, flash: { notice: exception.message }
   end
 end
